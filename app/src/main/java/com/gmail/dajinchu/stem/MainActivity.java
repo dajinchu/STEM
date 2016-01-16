c9eb3910c30ab47d9201dce87c8bcdbae6a46cde
Da-Jin Chu <dajinchu@gmail.com>
2016-01-16 17:29:36 -0500

    CheckIn and Progress in two tabs in MainActivity. Progress is placeholder fragment

--- app/src/main/java/com/gmail/dajinchu/stem/MainActivity.java
+++ app/src/main/java/com/gmail/dajinchu/stem/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gmail.dajinchu.stem;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,6 +10,8 @@ import android.view.Menu;
 public class MainActivity extends AppCompatActivity {
 
     public static HabitDbHelper dbHelper;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,38 @@ public class MainActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        CheckInFragment checkInFragment = new CheckInFragment();
+        viewPager = (ViewPager) findViewById(R.id.main_view_pager);
+        setupViewPager(viewPager);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.main_fragment_container, checkInFragment).commit();
+        tabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
         dbHelper = new HabitDbHelper(this);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new CheckInFragment(),"CHECK IN");
+        adapter.addFrag(new ViewProgressFragment(),"PROGRESS");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
---
+++ app/src/main/java/com/gmail/dajinchu/stem/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.gmail.dajinchu.stem;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Da-Jin on 1/16/2016.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter{
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager supportFragmentManager) {
+        super(supportFragmentManager);
+    }
+
+    public void addFrag(Fragment fragment, String title){
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
