7bb44a5a5110cc8a8e264a0d353a7dc79685a3aa
Da-Jin Chu <dajinchu@gmail.com>
2016-01-24 19:45:27 -0500

    Custom Title TextView to undo the stupid looking bold title

--- app/src/main/java/com/gmail/dajinchu/stem/MainActivity.java
+++ app/src/main/java/com/gmail/dajinchu/stem/MainActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 import java.util.Calendar;
@@ -22,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
 
         viewPager = (ViewPager) findViewById(R.id.main_view_pager);
         setupViewPager(viewPager);
